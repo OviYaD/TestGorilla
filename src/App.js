715280bdab4147fd4a9340281d0c41b350c740d3
OviYaD <oviya.d@codingmart.com>
2023-03-01 15:36:27 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Navbar from './components/NavBar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-    <Navbar></Navbar>
+    {/* <Navbar></Navbar> */}
+    <Footer></Footer>
     </div>
   );
 }
